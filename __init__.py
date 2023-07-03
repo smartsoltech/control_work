a68c15dda86cf9eb2c82a90e93ddd495c784035c
Andrew Choi <a.choi@smartsoltech.kr>
2023-07-04 01:31:12 +0900

    load_dotenv fixes

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv, find_dotenv
 from os import getenv
-load_dotenv(find_dotenv('.env.local'), '.env')
+load_dotenv('.env.local', '.env')
 
 DB_TYPE = getenv('DB_TYPE')
 DB_PATH = getenv('DB_PATH')+getenv('DB_NAME')
